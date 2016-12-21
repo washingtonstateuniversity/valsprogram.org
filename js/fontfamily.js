04a10a59d81c3bfa1a1172528fe99ecbe971c734
Charles J Dillon <charles.dillon2@wsu.edu>
2016-12-21 11:25:45 -0800

    Initial commit

---
+++ js/fontfamily.js
@@ -0,0 +1,13 @@
+WebFontConfig = {
+  google: { families: [ 'Rubik|400,400i,500,500i,700,700i' ] }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+
+})();
